a7aa054efd90b7fcc3f5240828ae3e3f2e141874
alex <saver2003@gmail.com>
2022-07-10 18:22:13 +0600

    Initial commit

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,31 @@
+const uuid = require('uuid')
+const {Device} = require('../models/models')
+const ApiError = require('../error/ApiError')
+const path = require('path')
+
+class DeviceController {
+  async create(req, res, next) {
+    // try {
+      const {name, price, brandId, typeId, info} = req.body
+      const {img} = req.files
+      let fileName = uuid.v4() + ".jpg"
+      await img.mv(path.resolve(__dirname, '..', 'static', fileName))
+
+      const device = await Device.create({name, price, brandId, typeId, img: fileName})
+
+      return res.json(device)
+    // } catch (e) {
+    //   next(ApiError.badRequest(e.message))
+    // }
+  }
+
+  async getAll(req, res) {
+
+  }
+
+  async getOne(req, res) {
+
+  }
+}
+
+module.exports = new DeviceController()
